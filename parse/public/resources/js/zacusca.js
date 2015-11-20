2ed9e9912a72e946b73d57339052644cb9b88610
grigoroiualex <grigoroiualexandru@gmail.com>
2015-11-20 08:55:23 +0200

    Am mai aranjat prima pagina si am adaugat optiune de login

--- parse/public/resources/js/zacusca.js
+++ parse/public/resources/js/zacusca.js
@@ -3,12 +3,13 @@ $(function() {
 
 	Parse.initialize("FkO54N3n6vn3gKijXwDlju0lZBaAjMXnqpvApCYF", "AF43qgGoJwDiDTELqmpc1H3QYuF2XVsNgvo81x0F");
 
-	$("#create-account-button").click(function() {
+	$("#signup-button").click(function() {
 		var firstname = $("#first-name").val();
 		var lastname = $("#last-name").val();
 		var username = $("#email").val();
 		var telephone = $("#telephone").val();
 		var password = $("#password").val();
+
 		Parse.User.signUp(username, password, {
 			ACL: new Parse.ACL(),
 			firstname: firstname,
@@ -21,6 +22,20 @@ $(function() {
 			},
 			error: function(user, error) {
 				Materialize.toast("Contul nu a putut fi creat", 5000);
+			}
+		});
+	});
+
+	$("#login-button").click(function() {
+		var username = $("#email").val();
+		var password = $("password").val();
+
+		Parse.User.logIn(username, password, {
+			success: function(user) {
+				Materialize.toast("Autentificare reușită", 5000);
+			},
+			error: function(user) {
+				Materialize.toast("Autentificare eșuată", 5000);
 			}
 		});
 	});
